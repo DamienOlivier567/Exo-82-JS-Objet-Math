691742ef32964f67b6704c17b2662348aee855ea
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2020-12-14 11:51:24 +0100

    Exo-82

---
+++ exo.js
@@ -0,0 +1,46 @@
+let value = 34.589456;
+console.log(Number.parseInt(value) + " = 34.589456");
+
+let value1 = 68.412784;
+console.log(Number.parseInt(value1) + " = 68.412784");
+
+let x = 39.456;
+console.log(Math.ceil(x) + " = 39.456");
+
+let x1 = 27.34876541;
+console.log(Math.ceil(x1) + " = 27.34876541");
+
+let y = 5.68
+console.log(Math.round(y) + " = 5.68");
+
+let y1 = 5.41
+console.log(Math.round(y1) + " = 5.41");
+
+let a = 6.12457;
+console.log(Number.parseInt(a) + " = 6.124257");
+
+let a1 = 9.568124;
+console.log(Number.parseInt(a1) + " = 9.568124");
+
+function mini (nombre1, nombre2, nombre3, nombre4, nombre5){
+
+    return nombre1, nombre2, nombre3, nombre4, nombre5;
+}
+
+function max (nbr1 , nbr2 , nbr3 , nbr4, nbr5){
+
+    return nbr1, nbr2, nbr3, nbr4, nbr5;
+}
+console.log(Math.min(mini(5,1,12,-1,45)));
+
+console.log(Math.min(mini(9,12,16,44,3)));
+
+console.log(Math.max(max(5,1,12,-1,45)));
+
+console.log(Math.max(max(9,12,16,44,3)));
+
+let b = -6;
+console.log(Math.abs(b) + " = -6");
+
+let b2 = -56;
+console.log(Math.abs(b2) + " = -56");
